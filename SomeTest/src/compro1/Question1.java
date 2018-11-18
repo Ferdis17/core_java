42e551f3f1d3fcc0d93bf14ed9ca3a99e22a5a3a
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-11-18 10:08:16 -0500

    some test
    core java

--- SomeTest/src/compro1/Question1.java
+++ SomeTest/src/compro1/Question1.java
@@ -4,14 +4,47 @@ public class Question1 {
 
 	public static void main(String[] args) {
 		
+		System.out.println(equvalentArrays(new int[] {0, 1, 2}, new int[] {2, 0, 1} ));
+		System.out.println(equvalentArrays(new int[] {0, 1, 2, 1}, new int[] {2, 0, 1} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
+//		System.out.println(equvalentArrays(new int[] {}, new int[] {} ));
 
 	}
-	public static int equivalentArrays(int [] a1, int [] a2) {
+	public static int equvalentArrays(int[] a1, int[] a2)
+	{
+		int equivalent=1;
 		
-		int equivalent = 1;
-		
-		
-		return 0;
+		for (int i = 0; i < a1.length&& equivalent==1; i++) {
+			int thereExits=0;
+			for (int j = 0; j < a2.length&&thereExits==0; j++) {
+				if(a1[i]==a2[j])
+					thereExits=1;
+				
+			}
+			if(thereExits==0)
+				equivalent=0;			
+			
+		}
+		for (int i = 0; i < a2.length&& equivalent==1; i++) {
+			int thereExits=0;
+			for (int j = 0; j < a1.length&&thereExits==0; j++) {
+				if(a2[i]==a1[j])
+					thereExits=1;
+				
+			}
+			if(thereExits==0)
+				equivalent=0;			
+			
+		}
+		return equivalent;
 	}
+
 
 }
