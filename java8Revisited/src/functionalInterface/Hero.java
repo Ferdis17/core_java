bcc833a99c3914189c8448c9f9bde4876fcf54bb
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-06-08 12:03:37 -0400

    predicate added

--- java8Revisited/src/functionalInterface/Hero.java
+++ java8Revisited/src/functionalInterface/Hero.java
@@ -1,10 +1,15 @@
 package functionalInterface;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Hero {
-	
+
 	private String name;
 	private boolean canFly;
-	
+
 	public Hero(String name, boolean canFly) {
 		super();
 		this.name = name;
@@ -26,8 +31,24 @@ public class Hero {
 	public void setCanFly(boolean canFly) {
 		this.canFly = canFly;
 	}
-	
-	
-	
+
+	public static List<String> getNamesMeetingCondition(List<Hero> heroList, Predicate<Hero> condition) {
+		List<String> foundNames = new ArrayList<>();
+		for (Hero hero : heroList) {
+			if (condition.test(hero)) {
+				foundNames.add(hero.name);
+			}
+		}
+		return foundNames;
+	}
+
+	public static void main(String[] aargs) {
+
+		List<Hero> heroes = Arrays.asList(new Hero("Hulk", false), new Hero("Superman", true),
+				new Hero("Batman", false));
+
+		List<String> result = getNamesMeetingCondition(heroes, h -> h.canFly);
+		result = getNamesMeetingCondition(heroes, h -> h.name.contains("man"));
+	}
 
 }
