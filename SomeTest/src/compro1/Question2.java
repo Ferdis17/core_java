4b131f454b62dc0197ebbcfb721bd5db55b4b17c
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-06-09 21:46:32 -0400

    refactoring

--- SomeTest/src/compro1/Question2.java
+++ SomeTest/src/compro1/Question2.java
@@ -7,14 +7,19 @@ public class Question2 {
 		
 		System.out.println(is123Array(new int [] {1, 2, 3, 3, 2, 1} ));
 		System.out.println(is123Array(new int [] {0, 1, 2, 3, 1, 2, 3} ));
+		System.out.println(is123Array(new int [] {1, 2, 3, 3, 2, 1, 1, 2, 3} ));
 		System.out.println(is123Array(new int [] {1, 2, 3} ));
+		System.out.println(is123Array(new int [] {} ));
+		
 		
 	}
 	
 	public static int is123Array(int [] a) {
-        if(a == null || a.length < 3 || a.length % 3 != 0) {
-            return 0;
-        }
+        if(a == null)
+        		return 0;
+        else if(a.length < 3 || a.length % 3 != 0)
+        		return 0;
+        
         int k = 0;
         while (3*k + 2 < a.length) {
             if (a[3*k] != 1 || a[3*k + 1] != 2 || a[3*k + 2] != 3 ) {
