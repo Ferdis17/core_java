ebc91f2e0df379b2e763895954f4f779004a028d
Ferdis17 <fmuriyesu@gmail.com>
2019-12-24 08:18:08 -0500

    graph

--- treesAndGraphs/src/bst/TreeNode.java
+++ treesAndGraphs/src/bst/TreeNode.java
@@ -1,30 +1,38 @@
 package bst;
 
-public class TreeNode<E>{
+public class TreeNode<E> {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	private E element;
 	private TreeNode<E> left;
 	private TreeNode<E> right;
-	
-	public TreeNode(E e) {
-		
-		this.element = e;
-		
-	}
-	
-	TreeNode<Integer> root = new TreeNode<>(60);
-//	root.left = new TreeNode<>(55);
-//	root.right = new TreeNode<>(100);
 
-	
-public boolean searchElement(E y) {
-	TreeNode<Integer> root = new TreeNode<>(this.element);
-	TreeNode<E> current = root;
-	return false;
-}
+	public TreeNode(E e) {
+
+		this.element = e;
+
+	}
+
+	TreeNode<Integer> root = new TreeNode<>(60);
+	// root.left = new TreeNode<>(55);
+	// root.right = new TreeNode<>(100);
+
+	public boolean searchElement(E y) {
+		TreeNode<Integer> root = new TreeNode<>(60);
+		TreeNode<E> current = (TreeNode<E>) root;
+
+		while (current != null) {
+			if (y < current.y) {
+				current = current.left;
+			} else if (y > current.y) {
+				current = current.right;
+			} else
+				return true;
+		}
+		return false;
+	}
 }
